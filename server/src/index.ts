c8c5adafe37c8cdcc2d79f5735c2fb872ed0069b
Onlyakr <onlyakr2477@gmail.com>
2026-04-07 19:31:35 +0700

    docs(server): add README.md

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import env from "./config/env.js";
 import postsRouter from "./modules/posts/posts.routes.js";
 import mockData from "./utils/mock-data.js";
 
-import { testMiddleware } from "./middlewares/test.js";
+import { authMiddleware } from "./middlewares/auth.js";
 
 import type { Request, Response } from "express";
 
@@ -24,90 +24,90 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use("/posts", testMiddleware, postsRouter);
+app.use("/posts", authMiddleware, postsRouter);
 
 // Health check endpoint
 app.get("/", (req: Request, res: Response) => {
-	res.json({ message: "Server is running" });
+  res.json({ message: "Server is running" });
 });
 
 // HTTP method ( GET, POST, PUT, PATCH and DELETE )
 app.get("/items", (req: Request, res: Response) => {
-	// Query parameter
-	const { name } = req.query;
+  // Query parameter
+  const { name } = req.query;
 
-	if (!name) {
-		res.json(mockData);
-	}
+  if (!name) {
+    res.json(mockData);
+  }
 
-	res.json(mockData.filter((item) => item.name.includes(name as string)));
+  res.json(mockData.filter((item) => item.name.includes(name as string)));
 });
 
 // Route parameter
 app.get("/items/:id", (req: Request, res: Response) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	res.json(mockData.find((item) => item.id === Number(id)));
+  res.json(mockData.find((item) => item.id === Number(id)));
 });
 
 app.post("/items", (req: Request, res: Response) => {
-	const body = req.body;
+  const body = req.body;
 
-	mockData.push(body);
+  mockData.push(body);
 
-	res.send(201).json({
-		message: "Created",
-		data: body,
-	});
+  res.send(201).json({
+    message: "Created",
+    data: body,
+  });
 });
 
 app.put("/items/:id", (req: Request, res: Response) => {
-	const { id } = req.params;
-	const body = req.body;
+  const { id } = req.params;
+  const body = req.body;
 
-	const index = mockData.findIndex((item) => item.id === Number(id));
-	if (index !== -1) {
-		mockData[index] = { ...mockData[index], ...body };
-		res.json({
-			message: "Updated",
-			data: mockData[index],
-		});
-	} else {
-		res.send(404).json({
-			message: "Not found",
-		});
-	}
+  const index = mockData.findIndex((item) => item.id === Number(id));
+  if (index !== -1) {
+    mockData[index] = { ...mockData[index], ...body };
+    res.json({
+      message: "Updated",
+      data: mockData[index],
+    });
+  } else {
+    res.send(404).json({
+      message: "Not found",
+    });
+  }
 });
 
 app.patch("/items/:id", (req: Request, res: Response) => {
-	const { id } = req.params;
-	const body = req.body;
+  const { id } = req.params;
+  const body = req.body;
 
-	const index = mockData.findIndex((item) => item.id === Number(id));
-	if (index !== -1) {
-		mockData[index] = { ...mockData[index], ...body };
-		res.json({
-			message: "Updated",
-			data: mockData[index],
-		});
-	} else {
-		res.send(404).json({
-			message: "Not found",
-		});
-	}
+  const index = mockData.findIndex((item) => item.id === Number(id));
+  if (index !== -1) {
+    mockData[index] = { ...mockData[index], ...body };
+    res.json({
+      message: "Updated",
+      data: mockData[index],
+    });
+  } else {
+    res.send(404).json({
+      message: "Not found",
+    });
+  }
 });
 
 app.delete("/items/:id", (req: Request, res: Response) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const data = mockData.filter((data) => data.id !== Number(id));
+  const data = mockData.filter((data) => data.id !== Number(id));
 
-	res.json({
-		message: "Deleted",
-		data,
-	});
+  res.json({
+    message: "Deleted",
+    data,
+  });
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
